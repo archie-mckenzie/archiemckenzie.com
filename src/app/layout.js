4c6c2aa464b4ac1375a725f880a6f238cad28f9c
Archie McKenzie <archiem@princeton.edu>
2024-01-02 17:56:48 +0000

    emoji

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,12 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
+  const sfEmoji = getSFEmoji()
+
   return (
     <html lang="en">
-      <head><link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${getSFEmoji()}</text></svg>`} /></head>
+      <head><link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sfEmoji}</text></svg>`} /></head>
       <body>{children}</body>
     </html>
   )

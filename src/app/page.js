b9712bcd6d8f00e9285a0418dd6f1d8ad3c68e1f
archie-mckenzie <archie@generaltranslation.com>
2024-03-19 23:24:04 -0400

    work update

--- src/app/page.js
+++ src/app/page.js
@@ -6,19 +6,21 @@ export default function ArchieMcKenzie() {
     <>
       <Title />
       <main>
-      <p>Computer Science '24<br/>+ Ancient Greek<br/>@ Princeton</p>
+      <p>
+        I work at <Link href='https://generaltranslation.com' target="_blank">General Translation</Link> as the CEO.
+      </p>
       <p className='link-paragraph'>
         <Link href="/about">About</Link>
         <br/>
-        <Link href="https://archiemckenzie.substack.com" target="_blank">Ideas</Link>
+        <Link href="https://archiemckenzie.substack.com" target="_blank">Writing</Link>
         <br/>
         <Link href='https://codex.ai' target="_blank">Codex</Link>
-        <br/>
-        <Link href='https://linkedin.com/in/archie-mckenzie' target="_blank">LinkedIn</Link>
         <br/>
         <Link href='https://twitter.com/aksmckenzie' target="_blank">Twitter</Link>
         <br/>
         <Link href='https://github.com/archie-mckenzie' target="_blank">GitHub</Link>
+        <br/>
+        <Link href='https://linkedin.com/in/archie-mckenzie' target="_blank">LinkedIn</Link>
       </p>
       <p className='contact-paragraph'><a href='mailto:contact@archiemckenzie.com'>Get in touch!</a></p>
       <p className="hidden-text">

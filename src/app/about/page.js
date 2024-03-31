13c3f56b18587e422e32e86e2d25119c69a05b79
archie-mckenzie <archie@generaltranslation.com>
2024-03-30 22:01:51 -0400

    minor bio update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,7 +8,7 @@ export default function ArchieMcKenzie() {
       <main>
       <ul>
         <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as New Zealand and Kenya</li>
-        <li><Link href='documents/balliol.pdf' target="_blank">Turned down</Link> Classics at Oxford for Princeton CS</li>
+        <li><Link href='documents/balliol.pdf' target="_blank">Turned down</Link> Classics at Oxford for Computer Science at Princeton</li>
         <li><Link href='https://en.wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24</li>
         <li>Worked for <Link href='https://humanprogress.org' target="_blank">Human Progress</Link> and <Link href="https://pessimistsarchive.org/" target="_blank">Pessimists Archive</Link></li>
         <li>CEO of <Link href='https://generaltranslation.com' target="_blank">General Translation</Link>, which I co-founded with <Link href='https://www.linkedin.com/in/fernando-aviles/' target="_blank">Fernando Avilés-García</Link>.</li>

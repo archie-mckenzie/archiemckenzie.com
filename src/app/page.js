13251cbc90418927f051ec14de668fa9ebf1d635
archie-mckenzie <archie@generaltranslation.com>
2024-04-01 00:18:01 -0400

    ideas

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function ArchieMcKenzie() {
       <p className='link-paragraph'>
         <Link href="/about">About</Link>
         <br/>
-        <Link href="https://archiemckenzie.substack.com" target="_blank">Thoughts</Link>
+        <Link href="https://archiemckenzie.substack.com" target="_blank">Ideas</Link>
         <br/>
         <Link href='https://codex.ai' target="_blank">Codex</Link>
         <br/>

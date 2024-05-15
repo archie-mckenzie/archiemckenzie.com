7edc3ce854dfb3eb0f79059599f5c274f414f66d
archie-mckenzie <archie@generaltranslation.com>
2024-05-15 12:30:22 -0400

    consistent punctuation

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -19,14 +19,14 @@ export default function ArchieMcKenzie() {
       </header>
       <main>
       <ul>
-        <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as New Zealand and Kenya</li>
-        <li><Link href='documents/balliol.pdf' target="_blank">Turned down</Link> Classics at Oxford for <span className="link-like" onClick={handleClick}>Computer Science</span> at Princeton</li>
+        <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as New Zealand and Kenya.</li>
+        <li><Link href='documents/balliol.pdf' target="_blank">Turned down</Link> Classics at Oxford for <span className="link-like" onClick={handleClick}>Computer Science</span> at Princeton.</li>
         { 
           showFullDegree &&
-          <li>A.B. <Link href='https://www.cs.princeton.edu/' target="_blank">Computer Science</Link>, with certificates in <Link href='https://classics.princeton.edu/' target="_blank">Classical Greek</Link>, <Link href='https://hpd.princeton.edu/' target="_blank">History and the Practice of Diplomacy</Link></li>
+          <li>A.B. <Link href='https://www.cs.princeton.edu/' target="_blank">Computer Science</Link>, with certificates in <Link href='https://classics.princeton.edu/' target="_blank">Classical Greek</Link>, <Link href='https://hpd.princeton.edu/' target="_blank">History and the Practice of Diplomacy</Link>.</li>
         }
-        <li><Link href='https://en.wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24</li>
-        <li>Worked for <Link href='https://humanprogress.org' target="_blank">Human Progress</Link> and <Link href="https://pessimistsarchive.org/" target="_blank">Pessimists Archive</Link></li>
+        <li><Link href='https://en.wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24.</li>
+        <li>Worked for <Link href='https://humanprogress.org' target="_blank">Human Progress</Link> and <Link href="https://pessimistsarchive.org/" target="_blank">Pessimists Archive</Link>.</li>
         <li>CEO of <Link href='https://generaltranslation.com' target="_blank">General Translation</Link>, which I co-founded with <Link href='https://www.linkedin.com/in/fernando-aviles/' target="_blank">Fernando Avilés-García</Link>.</li>
       </ul>
       <Link href='/'>←</Link>

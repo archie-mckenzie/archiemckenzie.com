d2dad6c4913f013ba6c6fa666d78949dada351d1
archie-mckenzie <archie@generaltranslation.com>
2024-06-19 19:48:05 +0100

    gt_config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './stylesheet.css'
-import getSFEmoji from '../../js/sf'
+import getSFEmoji from '../js/sf'
 
 export const metadata = {
   title: 'Archie McKenzie'
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
 
   const sfEmoji = getSFEmoji();
   return (
-    <html lang="en">
+    <html>
       <head><link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sfEmoji}</text></svg>`} /></head>
       <body>{children}</body>
     </html>

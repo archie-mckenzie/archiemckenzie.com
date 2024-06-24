6545840b9d63d14568bdf78201d841a7e10866bc
archie-mckenzie <archie@generaltranslation.com>
2024-06-24 13:19:05 +0100

    remove localhost:10000

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,6 @@ export default function ArchieMcKenzie() {
       </p>
       <p className='contact-paragraph'>
         <Link href='/blog'>Blog</Link>
-        <Exclude label='123'></Exclude>
       </p>
       <p className='contact-paragraph'>
         <a href='mailto:contact@archiemckenzie.com'>Get in touch!</a>

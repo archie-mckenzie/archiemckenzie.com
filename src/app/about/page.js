363070b0bf539fd6935c952be968dc16cbb2fb0e
archie-mckenzie <archie@generaltranslation.com>
2024-08-07 12:53:22 -0700

    pbio edits

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,7 +12,7 @@ export default async function About() {
       <Title />
       <main>
       <ul>
-        <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as New Zealand and Kenya.</li>
+        <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as Kenya and New Zealand.</li>
         <GTProvider dictionary={{
           "education.expanded": <li><Variable label="B.A.">A.B. </Variable><Link href='/documents/degree.pdf' target="_blank">Computer Science</Link>, with certificates in <Link href='/documents/cert_clg.pdf' target="_blank">Classical Greek</Link>, <Link href='/documents/cert_hpd.pdf' target="_blank">History and the Practice of Diplomacy</Link>.</li>,
           "education": <li>Studied <span id='cs' className="link-like">Computer Science</span> at Princeton.</li>
@@ -21,7 +21,7 @@ export default async function About() {
         </GTProvider>
         <li><Link href='https://wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24.</li>
         <li>Worked for <Link href='https://humanprogress.org' target="_blank">Human Progress</Link> and <Link href="https://pessimistsarchive.org/" target="_blank">Pessimists Archive</Link>.</li>
-        <li>CEO of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>.</li>
+        <li>Founder of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>.</li>
       </ul>
       <BackArrow />
       </main>

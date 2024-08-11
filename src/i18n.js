68f9510b7bbce51de8d596f0e55e4a386151b7e5
archie-mckenzie <archie@generaltranslation.com>
2024-08-11 01:00:59 -0700

    specified render timeout

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,6 @@
 import { createGT } from 'gt-react/next'
 export const { T, GTProvider, Var } = createGT({
     defaultLocale: 'en',
+    renderTimeout: 8500,
     dictionaryName: "archiemckenzie.com"
 });

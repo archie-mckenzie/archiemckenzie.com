7af451278ed29e1a87bea8f4de91742b4fe68bbb
archie-mckenzie <archie@generaltranslation.com>
2024-08-11 21:58:51 -0700

    lower renderTimeout

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import { createGT } from 'gt-react/next'
 export const { T, GTProvider, Var } = createGT({
     defaultLocale: 'en',
-    renderTimeout: 8500,
+    renderTimeout: 6500,
     dictionaryName: "archiemckenzie.com"
 });

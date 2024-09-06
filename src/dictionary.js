a97abf0179929bcd0553ffed4c7e1d27a6442ae9
archie-mckenzie <archie@generaltranslation.com>
2024-09-06 11:39:35 -0700

    changed

--- src/dictionary.js
+++ src/dictionary.js
@@ -27,15 +27,17 @@ export default {
       </p>
       </>
     ),
-    "about": (
-        <ul>
-            <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as Kenya and New Zealand.</li>
-            <li>Studied <span id='cs' className="link-like">Computer Science</span> at Princeton.</li>
-            <Var name="expanding_item"></Var>
-            <li><Link href='https://wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24.</li>
-            <li>Worked for <Link href='https://humanprogress.org' target="_blank">Human Progress</Link> and <Link href="https://pessimistsarchive.org/" target="_blank">Pessimists Archive</Link>.</li>
-            <li>Founder of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>.</li>
-        </ul>
-    ),
+    "about": {
+        "background": <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as Kenya and New Zealand.</li>,
+        "education": (
+            <>
+                <li>Studied <span id='cs' className="link-like">Computer Science</span> at Princeton.</li>
+                <Var name="expanding_item"></Var>
+            </>
+        ),
+        "ta": <li><Link href='https://wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24.</li>,
+        "work": <li>Worked for <Link href='https://humanprogress.org' target="_blank">Human Progress</Link> and <Link href="https://pessimistsarchive.org/" target="_blank">Pessimists Archive</Link>.</li>,
+        "founder": <li>Founder of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>.</li>
+    },
     "expanding_education": <li><Var name="B.A.">A.B.</Var> <Link href='/documents/degree.pdf' target="_blank">Computer Science</Link>, with certificates in <Link href='/documents/cert_clg.pdf' target="_blank">Classical Greek</Link>, <Link href='/documents/cert_hpd.pdf' target="_blank">History and the Practice of Diplomacy</Link>.</li>
 }

877eed6cc6423ab66eb87504096876644e3cf700
archie-mckenzie <archie@generaltranslation.com>
2024-10-22 15:32:25 -0700

    params.locale

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -8,7 +8,7 @@ import { GTProvider } from 'gt-next';
 export default function Layout({ children, params }) {
   const sfEmoji = getSFEmoji();
   return (
-    <html dir={getLanguageDirection(params.locale)}>
+    <html lang={params.locale} dir={getLanguageDirection(params.locale)}>
       <head><link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sfEmoji}</text></svg>`} /></head>
       <body>
         <GTProvider>

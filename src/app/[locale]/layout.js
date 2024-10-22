204c530234549e098f2e34cd53d9593d13f9b33d
archie-mckenzie <archie@generaltranslation.com>
2024-10-22 15:36:34 -0700

    archie mckenzie

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -9,7 +9,10 @@ export default function Layout({ children, params }) {
   const sfEmoji = getSFEmoji();
   return (
     <html lang={params.locale} dir={getLanguageDirection(params.locale)}>
-      <head><link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sfEmoji}</text></svg>`} /></head>
+      <head>
+        <title>Archie McKenzie</title>
+        <link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sfEmoji}</text></svg>`} />
+      </head>
       <body>
         <GTProvider>
           {children}

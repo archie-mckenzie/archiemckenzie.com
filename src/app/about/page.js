1d0f9e01708410cb5ebdad513d7a9b0a66d9c441
archie-mckenzie <archie@generaltranslation.com>
2025-01-18 15:38:15 -0800

    removed unused imports

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,6 @@
 import Link from 'next/link'
 import Title from '../../components/Title';
 import BackArrow from '@/components/BackArrow';
-import ExpandingAbout from '@/components/ExpandingAbout';
 
 export default function About() {
 

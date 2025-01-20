0d0be41a0a9421ba461745826e378b7d1c480766
archie-mckenzie <archie@generaltranslation.com>
2025-01-19 16:27:20 -0800

    support for rtl languages and updated href

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,11 +4,13 @@ import './stylesheet.css'
 import getSFEmoji from '@/js/sf';
 import { getLocaleDirection } from 'generaltranslation';
 import { GTProvider } from 'gt-next';
+import { getLocale } from 'gt-next/server'
 
-export default function Layout({ children, params }) {
+export default async function Layout({ children }) {
   const sfEmoji = getSFEmoji();
+  const locale = await getLocale();
   return (
-    <html lang={params.locale} dir={getLocaleDirection(params.locale)}>
+    <html lang={locale} dir={getLocaleDirection(locale)}>
       <head>
         <title>Archie McKenzie</title>
         <link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sfEmoji}</text></svg>`} />

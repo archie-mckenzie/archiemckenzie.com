861c7233c099c64d10df66e8af4c0044ab403c6a
archie-mckenzie <archie@generaltranslation.com>
2025-01-19 16:33:43 -0800

    wording change

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function ArchieMcKenzie() {
               <li>Studied <span onClick={() => {setShowFullDegree(prev => !prev)}} className="link-like">Computer Science</span> at Princeton.</li>
               <Var>{ showFullDegree && <ExpandedEducation/> }</Var>
               <li><Link href='https://wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24.</li>
-              <li>Founder of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>, which helps developers make their software multilingual.</li>
+              <li>Founder of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>, which helps developers ship multilingual software.</li>
             </ul>
           </div>
           <p className='link-grid'>

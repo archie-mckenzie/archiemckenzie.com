3099c164f5d9c5977b31b742e79ec9f3ce6cceec
archie-mckenzie <archie@generaltranslation.com>
2025-01-26 13:14:41 -0800

    T

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,12 @@ export default function ArchieMcKenzie() {
             <ul>
               <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as Kenya and New Zealand.</li>
               <li>Studied <span onClick={() => {setShowFullDegree(prev => !prev)}} className="link-like">Computer Science</span> at Princeton.</li>
-              <Var>{ showFullDegree && <ExpandedEducation/> }</Var>
+              <Var>{ 
+                showFullDegree && 
+                <T id="expanded_education">
+                  <li><Var name="B.A.">A.B.</Var> <Link href='https://www.cs.princeton.edu/ugrad/undergraduate-program' target="_blank">Computer Science</Link>, with certificates in <Link href="https://classics.princeton.edu/programs/undergraduate/minors-certificates" target="_blank">Classical Greek</Link>, and <Link href="https://hpd.princeton.edu/" target="_blank">History and the Practice of Diplomacy</Link>.</li>
+                </T> 
+              }</Var>
               <li><Link href='https://wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24.</li>
               <li>Founder of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>, which helps developers ship multilingual software.</li>
             </ul>

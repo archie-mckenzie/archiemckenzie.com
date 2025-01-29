ab0ef576c537a600a2325600d98703cea1b3a5d5
archie-mckenzie <archie@generaltranslation.com>
2025-01-29 10:15:35 -0800

    added branch

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import Link from 'next/link.js';
 import Title from '../components/Title.js'
 
-import { T, Var } from 'gt-next';
+import { Branch, T, Var } from 'gt-next';
 import { useState } from 'react';
 
 export default function ArchieMcKenzie() {
@@ -20,12 +20,12 @@ export default function ArchieMcKenzie() {
             <ul>
               <li>Grew up in <Link href='https://en.wikipedia.org/wiki/Bath,_Somerset' target="_blank">Bath</Link>, England, as well as Kenya and New Zealand.</li>
               <li>Studied <span onClick={() => {setShowFullDegree(prev => !prev)}} className="link-like">Computer Science</span> at Princeton.</li>
-              <Var>{ 
-                showFullDegree && 
-                <T id="expanded_education">
+              <Branch 
+                branch={`${showFullDegree}`} 
+                true={
                   <li><Var name="B.A.">A.B.</Var> <Link href='https://www.cs.princeton.edu/ugrad/undergraduate-program' target="_blank">Computer Science</Link>, with certificates in <Link href="https://classics.princeton.edu/programs/undergraduate/minors-certificates" target="_blank">Classical Greek</Link>, and <Link href="https://hpd.princeton.edu/" target="_blank">History and the Practice of Diplomacy</Link>.</li>
-                </T> 
-              }</Var>
+                }
+              />
               <li><Link href='https://wikipedia.org/wiki/Brian_Kernighan' target='_blank'>Brian Kernighan</Link>'s TA from 2021-24.</li>
               <li>Founder of <Link href='https://generaltranslation.com' target="_blank">General Translation, Inc.</Link>, which helps developers ship multilingual software.</li>
             </ul>
@@ -46,19 +46,3 @@ export default function ArchieMcKenzie() {
     </>
   )
 }
-
-/*
-
-<p><b>Hello, world!</b></p>
-      <p>I'm currently working on <Link href='https://generaltranslation.com' target="_blank">General Translation</Link>, an AI localization platform.</p>
-      <p className='link-grid'>
-        <Link href="/about">About me</Link>
-        <Link href='https://github.com/archie-mckenzie' target="_blank">GitHub</Link>
-        <Link href='https://twitter.com/aksmckenzie' target="_blank">Twitter</Link>
-        <Link href='https://linkedin.com/in/archie-mckenzie' target="_blank">LinkedIn</Link>
-      </p>
-      <p className='contact-paragraph'>
-        <a href='mailto:contact@archiemckenzie.com'>Get in touch!</a>
-      </p>
-
-*/

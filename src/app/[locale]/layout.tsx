41dfb83176dbdc01944fa9910b25d91025efbcda
archie-mckenzie <archie@generaltranslation.com>
2026-05-18 12:25:06 -0700

    refreshed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,7 +12,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
         <title>Archie McKenzie</title>
         <link rel="icon" id="sf" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sfEmoji}</text></svg>`} />
       </head>
-      <body>
+      <body className="spacing-text">
         <GTProvider>
           {children}
         </GTProvider>
